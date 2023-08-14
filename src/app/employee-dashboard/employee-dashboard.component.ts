6ce4a04fca6681accc6a368bbaeed9685d021bda
Henry G Shoko <gshokx91@gmail.com>
2023-08-14 23:36:57 +0200

    employee details modal

--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-dashboard',
   templateUrl: './employee-dashboard.component.html',
-  styleUrls: ['./employee-dashboard.component.css']
+  styleUrls: ['./employee-dashboard.component.css'],
 })
 export class EmployeeDashboardComponent implements OnInit {
-
-  constructor() { }
+  formValue!: FormGroup;
+  constructor(private formbuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.formValue = this.formbuilder.group({
+      firstName: [''],
+      lastName: [''],
+      mobile: [''],
+      email: [''],
+      dateofbirth: [''],
+      streetaddress: [''],
+      city: [''],
+      postalcode: [''],
+      country: [''],
+      skill: [''],
+      yrsexp: [''],
+      seniorityrating: [''],
+    });
   }
-
 }
