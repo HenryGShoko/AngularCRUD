a376ecad267975eed74744a81b9275d9159a5768
Henry G Shoko <gshokx91@gmail.com>
2023-08-17 03:09:51 +0200

    login and signup components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [AppComponent, EmployeeDashboardComponent],
+  declarations: [AppComponent, EmployeeDashboardComponent, LoginComponent, SignupComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -8,7 +8,7 @@ import { map } from 'rxjs/operators';
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  postEmployee(data: any) {
+  PostEmployee(data: any) {
     return this.http.post<any>('http://localhost:3000/posts/', data).pipe(
       map((res: any) => {
         return res;
@@ -16,23 +16,25 @@ export class ApiService {
     );
   }
 
-  getEmployee() {
-    return this.http.get<any>('http://localhost:3000/posts/').pipe(
+  DeleteEmployee(id: number) {
+    return this.http.delete<any>('http://localhost:3000/posts/' + id).pipe(
       map((res: any) => {
         return res;
       })
     );
   }
 
-  updateEmployee(data: any, id: number) {
+
+  
+  UpdateEmployee(data: any, id: number) {
     return this.http.put<any>('http://localhost:3000/posts/' + id, data).pipe(
       map((res: any) => {
         return res;
       })
     );
   }
-  deleteEmployee(id: number) {
-    return this.http.delete<any>('http://localhost:3000/posts/' + id).pipe(
+  GetEmployees() {
+    return this.http.get<any>('http://localhost:3000/posts/').pipe(
       map((res: any) => {
         return res;
       })
