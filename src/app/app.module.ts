ed1cc40955f5a32913164eedc5c35a37072d8c69
Henry G Shoko <gshokx91@gmail.com>
2023-08-17 13:21:10 +0200

    Revert "login and signup components"
    This reverts commit a376ecad267975eed74744a81b9275d9159a5768.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,9 @@ import { AppComponent } from './app.component';
 import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [AppComponent, EmployeeDashboardComponent, LoginComponent, SignupComponent],
+  declarations: [AppComponent, EmployeeDashboardComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -8,7 +8,7 @@ import { map } from 'rxjs/operators';
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  PostEmployee(data: any) {
+  postEmployee(data: any) {
     return this.http.post<any>('http://localhost:3000/posts/', data).pipe(
       map((res: any) => {
         return res;
@@ -16,25 +16,23 @@ export class ApiService {
     );
   }
 
-  DeleteEmployee(id: number) {
-    return this.http.delete<any>('http://localhost:3000/posts/' + id).pipe(
+  getEmployee() {
+    return this.http.get<any>('http://localhost:3000/posts/').pipe(
       map((res: any) => {
         return res;
       })
     );
   }
 
-
-  
-  UpdateEmployee(data: any, id: number) {
+  updateEmployee(data: any, id: number) {
     return this.http.put<any>('http://localhost:3000/posts/' + id, data).pipe(
       map((res: any) => {
         return res;
       })
     );
   }
-  GetEmployees() {
-    return this.http.get<any>('http://localhost:3000/posts/').pipe(
+  deleteEmployee(id: number) {
+    return this.http.delete<any>('http://localhost:3000/posts/' + id).pipe(
       map((res: any) => {
         return res;
       })
